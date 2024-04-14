fee3c464d6d9a086530aeabc7de9edc75ee2e0b2
saladware <saladware46@gmai.com>
2024-04-14 08:18:16 +0300

    fix relationship

--- tinkoff_detox/tokens/models.py
+++ tinkoff_detox/tokens/models.py
@@ -1,9 +1,13 @@
+from typing import TYPE_CHECKING
 from uuid import UUID
 from datetime import datetime
 
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from ..database import Base
+
+if TYPE_CHECKING:
+    from ..users.models import User
 
 
 class Token(Base):
@@ -11,6 +15,7 @@ class Token(Base):
 
     id: Mapped[UUID] = mapped_column(primary_key=True)
     user_id: Mapped[UUID]
+    user: Mapped[User] = relationship(back_populates="tokens")
     token: Mapped[str] = mapped_column(unique=True)
     name: Mapped[str]
     created_at: Mapped[datetime]
