8a67fed0f6db2e9ac563578c4012c3fa4fa8f86c
saladware <saladware46@gmai.com>
2024-04-14 17:00:22 +0300

    comments

--- tinkoff_detox/model/routes.py
+++ tinkoff_detox/model/routes.py
@@ -5,32 +5,67 @@ from ..tokens.dependencies import TokenRequired
 from fastapi import APIRouter, UploadFile
 
 from .ml import detoxify
+from .parser import get_comments_by_article
 
 
 model = APIRouter()
 
 
 @model.post("/textFilter")
-def text_filter(promt: str, token: TokenRequired,
+def text_filter(
+    promt: str,
+    token: TokenRequired,
     max_tokens: int = 50,
     temperature: float = 0.7,
     top_k: int = 50,
     top_p: float = 0.95,
     penalty: float = 1.0,
-    n: int = 1, ):
+    n: int = 1,
+):
     return detoxify(promt, max_tokens, temperature, top_k, top_p, penalty, n)
 
 
 @model.post("/journalCommentsFilter")
-async def journal_comments_filter(path: str, token: TokenRequired, type: Literal["blur", "rewrite"]):
-    return path
+def journal_comments_filter(
+    path: str,
+    token: TokenRequired,
+    offset: int = 0,
+    limit: int = 10,
+    max_tokens: int = 50,
+    temperature: float = 0.7,
+    top_k: int = 50,
+    top_p: float = 0.95,
+    penalty: float = 1.0,
+    n: int = 1,
+):
+    comments = get_comments_by_article(path)
+    return [
+        {
+            "promt": comment,
+            "result": detoxify(comment, max_tokens, temperature, top_k, top_p, penalty, n)
+        }
+        for comment in comments
+    ]
+        
+
 
 
 @model.post("/audioFilter")
-async def audio_filter(file: UploadFile, token: TokenRequired, type: Literal["blur", "rewrite"]):
+async def audio_filter(
+    file: UploadFile,
+    token: TokenRequired,
+    max_tokens: int = 50,
+    temperature: float = 0.7,
+    top_k: int = 50,
+    top_p: float = 0.95,
+    penalty: float = 1.0,
+    n: int = 1,
+):
     return file.file
 
 
 @model.post("/videoFilter")
-async def video_filter(file: UploadFile, token: TokenRequired, type: Literal["blur", "rewrite"]):
+async def video_filter(
+    file: UploadFile, token: TokenRequired, type: Literal["blur", "rewrite"]
+):
     return file.file
